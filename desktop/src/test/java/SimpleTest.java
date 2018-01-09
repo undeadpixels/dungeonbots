d7db48e1976c838a3d1f2c8af3e5827f80d402c3
Kevin Parker <kevin.m.parker@gmail.com>
2018-01-09 12:11:32 -0700

    added a gradle test

---
+++ desktop/src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		System.out.println("A test was run.");
+		org.junit.Assert.assertTrue(true);
+	}
+
+}
