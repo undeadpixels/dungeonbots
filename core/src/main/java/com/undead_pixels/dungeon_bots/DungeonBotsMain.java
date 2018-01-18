ba73b88f4e1dadbc4d88bd803e1ee78007276d05
Wesley Oates <wesley.oates@gmail.com>
2018-01-18 13:12:01 -0700

    Struggling with layout system.  Urgh GDX

--- core/src/main/java/com/undead_pixels/dungeon_bots/DungeonBotsMain.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/DungeonBotsMain.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle;
@@ -48,240 +49,7 @@ public class DungeonBotsMain extends Game {
 		setScreen(new NullScreen());
 	}
 
-	/**
-	 * A Menu participates in layout.
-	 * 
-	 * @author Wesley Oates
-	 */
-	public class Menu extends Widget {
-
-		
-		private float _ColumnWidth;
-		private float _PrefWidth, _PrefHeight;
-		private ClickListener _ClickListener;
-		
-		
-		/** Menu tree structure. */
-		private MenuNode _RootNode;
-
-		/** A Menu has its own stage so it can prioritize input handling. */
-		// private Stage _Stage = new Stage();
-		private Skin _Skin;
-
-		public Menu() {
-			this(null);			
-		}
-		public Menu(Skin skin){
-			_RootNode = new MenuNode(null);
-			_RootNode.open();			
-		}
-
-		/// MENU CONTENT CONTROLS
-		protected class MenuNode {
-
-			/** True if the MenuNode is open, false if not. */
-			private boolean _IsOpen = false;
-			private Object _Contents;
-			private Hashtable<Object, MenuNode> _SubNodes = null;
-			private Function<Object, Boolean> _EnabledTester;
-			private Consumer<Object> _Executor;
-
-			public MenuNode(Object contents) {
-				this(contents, obj -> true, mn -> System.out.println(contents.toString()));
-			}
-
-			public MenuNode(Object contents, Function<Object, Boolean> enabledTester, Consumer<Object> executor) {
-				_Contents = contents;
-				_EnabledTester = enabledTester;
-				_Executor = executor;
-			}
-
-			/**
-			 * Two nodes compared are equal if their contents are equal. For
-			 * other types, a node is equal to another object if its contents
-			 * are equal to that object.
-			 */
-			@Override
-			public boolean equals(Object other) {
-				if (this == other)
-					return true;
-				if (other instanceof MenuNode) {
-					final MenuNode that = (MenuNode) other;
-					return this._Contents.equals(that._Contents);
-				}
-				return this._Contents.equals(other);
-			}
-
-			/** Nodes hash by their contents. */
-			@Override
-			public int hashCode() {
-				return this._Contents.hashCode();
-			}
-
-			/**
-			 * Returns the MenuNode at the end of the specified route. If the
-			 * route does not lead to a valid node, returns null.
-			 */
-			public MenuNode getNode(java.util.List<Object> route) {
-				if (route == null)
-					throw new IllegalArgumentException("Route cannot be a null list of objects.");
-				if (route.isEmpty())
-					return this;
-				if (_SubNodes == null)
-					return null;
-				final MenuNode subMenu = _SubNodes.get(route.get(0));
-				return subMenu == null ? null : subMenu.getNode(route.subList(1, route.size()));
-			}
-
-			/**
-			 * Returns the MenuNode at the end of the specified string path,
-			 * whose levels are delimited by '/' characters. If the route does
-			 * not lead to a valid node, returns null.
-			 */
-			public MenuNode getNode(String route) {
-				ArrayList<Object> listRoute = new ArrayList<Object>();
-				for (String str : route.split("/"))
-					listRoute.add(str);
-				return getNode(listRoute);
-			}
-
-			/**
-			 * Ensures that nodes along the '/'-delimited given route exist, and
-			 * returns the very last node in the route.
-			 */
-			public MenuNode addNode(String route) {
-
-				ArrayList<Object> listRoute = new ArrayList<Object>();
-				for (String str : route.split("/"))
-					listRoute.add(str);
-				return addNode(listRoute);
-			}
-
-			/**
-			 * Ensures that the specified node tree route exists, and returns
-			 * the very last node in the route.
-			 */
-			public MenuNode addNode(java.util.List<Object> route) {
-
-				// Corner case - bad input.
-				if (route == null)
-					throw new IllegalArgumentException("Route cannot be a null input.");
-
-				// If we're at the leaf end of the route, return this.
-				if (route.isEmpty())
-					return this;
-
-				// Are we sitting at an existing leaf in the tree? If so, allow
-				// for branches.
-				if (_SubNodes == null)
-					_SubNodes = new Hashtable<Object, MenuNode>();
-
-				// Is there a route down from here? If not, add it.
-				MenuNode subMenu = _SubNodes.get(route.get(0));
-				if (subMenu == null) {
-					subMenu = new MenuNode(route.get(0));
-					_SubNodes.put(subMenu._Contents, subMenu);
-				}
-
-				// Return the result of the next item in the tree.
-				return subMenu.addNode(route.subList(1, route.size()));
-			}
-
-			/**
-			 * Ensures that nodes along the given '/' - delimited route exist,
-			 * and ensures a node containing the given contents exists at the
-			 * end. The node will follow the given anonymous functions for
-			 * determining whether the node is enabled or not, and for execution
-			 * when the node is clicked.
-			 */
-			public MenuNode addNode(java.util.List<Object> route, Object newContents,
-					Function<Object, Boolean> enabledTester, Consumer<Object> executor) {
-				MenuNode node = addNode(route);
-				MenuNode child = node._SubNodes.get(newContents);
-				if (child != null)
-					return child;
-				child = new MenuNode(newContents, enabledTester, executor);
-				node._SubNodes.put(child._Contents, child);
-				return child;
-			}
-
-			/**
-			 * Ensures that the specified node tree route exists, and returns
-			 * the very last node in the route.
-			 */
-			public MenuNode addNode(String route, Object newContents, Function<Object, Boolean> enabledTester,
-					Consumer<Object> executor) {
-				ArrayList<Object> listRoute = new ArrayList<Object>();
-				for (String str : route.split("/"))
-					listRoute.add(str);
-				return addNode(listRoute, newContents, enabledTester, executor);
-			}
-
-			/**
-			 * Activates the defined executor established when this node was
-			 * created.
-			 */
-			public void execute() {
-				if (_EnabledTester.apply(_Contents))
-					_Executor.accept(_Contents);
-			}
-
-			/**Marks as closed this node and all children of this node.*/
-			public void close() {
-				this._IsOpen = false;
-				this.closeChildren();
-			}
-
-			/**Marks as closed all children of this node.*/
-			public void closeChildren() {
-				for (MenuNode child : _SubNodes.values()) {
-					child.close();
-				}
-			}
-
-			/**Returns whether the given node is a child of this node.*/
-			public boolean contains(MenuNode node) {
-				return _SubNodes.containsValue(node);
-			}
-
-			/**Returns whether the given object is contained as the contents of a child of this node.*/
-			public boolean contains(Object obj) {
-				return (obj.equals(this._Contents)) || _SubNodes.containsKey(obj);				
-			}
-			
-			public void open(){
-				_IsOpen = true;
-			}
-		}
-
-		
-		public boolean addItem(String path, Object newContents, Function<Object, Boolean> enabledTester,
-				Consumer<Object> executor) {
-			MenuNode node = _RootNode.addNode(path, newContents, enabledTester, executor);
-			return node != null;
-		}
-		
-		
-		
-
-	}
 	
-	/**The style for a menu*/
-	static public class MenuStyle extends SelectBoxStyle{
-		public int ColumnWidth;
-		
-		public MenuStyle(int columnWidth, BitmapFont font, Color fontColor, Drawable background, ScrollPaneStyle scrollStyle, ListStyle listStyle){
-			super(font, fontColor, background, scrollStyle, listStyle);
-			ColumnWidth = columnWidth;
-		}
-		
-		public MenuStyle(MenuStyle originalStyle){
-			super(originalStyle);
-			this.ColumnWidth = originalStyle.ColumnWidth;
-		}
-	}
-	
-
 	/**
 	 * This will be deleted eventually, but it at least allows us to have a fake
 	 * screen
@@ -302,33 +70,24 @@ public class DungeonBotsMain extends Game {
 		public void create() {
 			Gdx.input.setInputProcessor(stage);
 
-			// Relying on tutorials at
-			// https://github.com/libgdx/libgdx/blob/master/tests/gdx-tests/src/com/badlogic/gdx/tests/UISimpleTest.java#L37
-
-			Pixmap whitePM = new Pixmap(1, 1, Format.RGBA8888);
-			whitePM.setColor(Color.WHITE);
-			whitePM.fill();
-			skin.add("white", new Texture(whitePM));
+			
+			
 
 			// Not sure what a table does vis-a-vis a stage...
 			table.setFillParent(true);
 			stage.addActor(table);
 			table.setDebug(true);
 
-			skin.add("defaultFont", new BitmapFont()); // Use the default font
-														// for now.
 
-			// Set up the standard textbutton style.
-			TextButtonStyle style = new TextButtonStyle();
-			style.up = skin.newDrawable("white", Color.DARK_GRAY);
-			style.down = skin.newDrawable("white", Color.DARK_GRAY);
-			style.checked = skin.newDrawable("white", Color.GREEN);
-			style.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-			style.font = skin.getFont("defaultFont");
-			skin.add("default", style);
-
-			TextButton button1 = new TextButton("Here!", style);
-			table.add(button1);
+			//Create and add a menu.			
+			MenuStyle menuStyle = new MenuStyle();
+			menuStyle.columnWidth=20;
+			Menu menu = new Menu(menuStyle);
+			table.add(menu);
+			
+			menu.addItem("", "File", null, null);
+			menu.addItem("File",  "Open", null, null);
+			menu.addItem("Path",  "Save",  null, null);
 
 		}
 
