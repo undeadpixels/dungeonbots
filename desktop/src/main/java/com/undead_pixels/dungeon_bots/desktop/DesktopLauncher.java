8590bd91e2431e95696f1b8e4de10bb5c65876b7
Kevin Parker <kevin.m.parker@gmail.com>
2018-01-18 13:18:43 -0700

    moved lwjgl context into jframe

--- desktop/src/main/java/com/undead_pixels/dungeon_bots/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/undead_pixels/dungeon_bots/desktop/DesktopLauncher.java
@@ -1,5 +1,8 @@
 package com.undead_pixels.dungeon_bots.desktop;
 
+import javax.swing.JFrame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglAWTCanvas;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.undead_pixels.dungeon_bots.DungeonBotsMain;
@@ -7,8 +10,17 @@ import com.undead_pixels.dungeon_bots.DungeonBotsMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		// TODO - construct JFrame
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(DungeonBotsMain.instance, config);
+		JFrame frame = new JFrame("DungeonBots");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // TODO - deleteme and replace with some kind of listener for saving
+		
+		LwjglAWTCanvas canvas = new LwjglAWTCanvas(DungeonBotsMain.instance);
+		frame.getContentPane().add(canvas.getCanvas());
+		frame.pack();
+		frame.setVisible(true);
+		frame.setSize(1024, 768);
+		
+		//LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//new LwjglApplication(DungeonBotsMain.instance, config);
 	}
 }
  
