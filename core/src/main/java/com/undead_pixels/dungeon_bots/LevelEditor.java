66996093a486c5f553293450837ce2fe50a85cbe
Wesley Oates <wesley.oates@gmail.com>
2018-01-22 11:04:18 -0700

    Level editor skeleton started.

---
+++ core/src/main/java/com/undead_pixels/dungeon_bots/LevelEditor.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.undead_pixels.dungeon_bots;
+
+import com.undead_pixels.dungeon_bots.ui.GDXandSwingScreen;
+
+/**
+ * @author Wesley
+ *
+ */
+public class LevelEditor extends GDXandSwingScreen {
+
+	/**
+	 * 
+	 */
+	public LevelEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
