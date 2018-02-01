7b1c61491a5989e981ef943645633a12df479647
lennyferguson <trio.of.four@gmail.com>
2018-02-01 13:01:26 -0700

    Slightly modified LuaSandbox script from file method.

--- core/src/main/java/com/undead_pixels/dungeon_bots/scene/World.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/scene/World.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.undead_pixels.dungeon_bots.scene.entities.Actor;
 import com.undead_pixels.dungeon_bots.scene.entities.Entity;
 import com.undead_pixels.dungeon_bots.scene.entities.Player;
 import com.undead_pixels.dungeon_bots.scene.entities.Tile;
@@ -153,7 +155,6 @@ public class World implements GetLuaFacade, GetLuaSandbox {
 		this.name = name;
    	 	backgroundImage = null;
    	 	tiles = new Tile[0][0];
-
 		
 		if(luaScriptFile != null) {
 			tileTypesCollection = new TileTypes();
