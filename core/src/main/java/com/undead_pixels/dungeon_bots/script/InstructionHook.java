4ca67e12f602a261cc56d944d5278cc58363d048
Kevin Parker <kevin.m.parker@gmail.com>
2018-02-03 14:37:45 -0700

    InstructionHook attribution

--- core/src/main/java/com/undead_pixels/dungeon_bots/script/InstructionHook.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/script/InstructionHook.java
@@ -3,6 +3,9 @@ package com.undead_pixels.dungeon_bots.script;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.DebugLib;
 
+/**
+ * Based on https://stackoverflow.com/questions/17496868/lua-java-luaj-handling-or-interrupting-infinite-loops-and-threads?noredirect=1&lq=1
+ */
 public class InstructionHook extends DebugLib {
     public boolean interrupted = false;
 
