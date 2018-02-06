3de87223b17c81587495d194e089497645ced213
Kevin Parker <kevin.m.parker@gmail.com>
2018-02-06 14:41:29 -0700

    opens dungeonbots website from clicking 'community' button

--- core/src/main/java/com/undead_pixels/dungeon_bots/ui/screens/CommunityScreen.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/ui/screens/CommunityScreen.java
@@ -1,8 +1,23 @@
 package com.undead_pixels.dungeon_bots.ui.screens;
 
 import java.awt.Container;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class CommunityScreen extends Screen {
+
+	public static final URI homeURI;
+	
+	static {
+		URI homeURItmp;
+		try {
+			homeURItmp = new URI("https://dungeonbots.herokuapp.com");
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			homeURItmp = null;
+		}
+		homeURI = homeURItmp;
+	}
 
 	public CommunityScreen() {
 		// TODO Auto-generated constructor stub
