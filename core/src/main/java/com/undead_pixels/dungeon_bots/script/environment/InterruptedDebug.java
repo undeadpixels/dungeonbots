c625dc3221bac3f53c3c779ceedaa1c2a6d208df
lennyferguson <trio.of.four@gmail.com>
2018-02-13 15:52:10 -0700

    Created basic HookFunction type

--- core/src/main/java/com/undead_pixels/dungeon_bots/script/environment/InterruptedDebug.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/script/environment/InterruptedDebug.java
@@ -2,15 +2,23 @@ package com.undead_pixels.dungeon_bots.script.environment;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.DebugLib;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * Code derived from Stackoverflow response to handling 'terminating' Lua scripts<br>
  * <a>https://stackoverflow.com/questions/17496868/lua-java-luaj-handling-or-interrupting-infinite-loops-and-threads?noredirect=1&lq=1</a>
  */
 public class InterruptedDebug extends DebugLib {
 	public boolean interrupted = false;
-
+	public final Semaphore yeildLock = new Semaphore(1, true);
 	@Override
 	public void onInstruction(int pc, Varargs v, int top) {
+		try {
+			yeildLock.acquire();
+		}
+		catch (InterruptedException ie) {
+			throw new ScriptInterruptException();
+		}
 		if (interrupted) {
 			throw new ScriptInterruptException();
 		}
