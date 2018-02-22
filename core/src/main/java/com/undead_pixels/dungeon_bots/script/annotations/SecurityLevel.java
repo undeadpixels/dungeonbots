ca3fae63948a1a0d914f8403d6f69503b0698c0e
Kevin Parker <kevin.m.parker@gmail.com>
2018-02-22 16:07:24 -0700

    perhaps fixed the non-separate Globals per sandbox issue (code by Stewart)

--- core/src/main/java/com/undead_pixels/dungeon_bots/script/annotations/SecurityLevel.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/script/annotations/SecurityLevel.java
@@ -1,6 +1,9 @@
 package com.undead_pixels.dungeon_bots.script.annotations;
 
 import com.undead_pixels.dungeon_bots.script.environment.GameGlobals;
+
+import java.util.function.Supplier;
+
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.lib.jse.JsePlatform;
 
@@ -8,15 +11,19 @@ import org.luaj.vm2.lib.jse.JsePlatform;
  * Enumerate different security levels for a DungeonBots user in a Script Environment
  */
 public enum SecurityLevel {
-    DEBUG(3, JsePlatform.debugGlobals()),
-    AUTHOR(2, JsePlatform.standardGlobals()),
-    DEFAULT(1, GameGlobals.playerGlobals()),
-    NONE(0, JsePlatform.standardGlobals());
+    DEBUG(3, () -> JsePlatform.debugGlobals()),
+    AUTHOR(2, () -> JsePlatform.standardGlobals()),
+    DEFAULT(1, () -> GameGlobals.playerGlobals()),
+    NONE(0, () -> JsePlatform.standardGlobals());
     public final int level;
-    public final Globals globals;
+    public final Supplier<Globals> globalsSupplier;
 
-    SecurityLevel(int level, Globals globals) {
+    SecurityLevel(int level, Supplier<Globals> globals) {
         this.level = level;
-        this.globals = globals;
+        this.globalsSupplier = globals;
+    }
+    
+    public Globals getGlobals() {
+    		return this.globalsSupplier.get();
     }
 }
