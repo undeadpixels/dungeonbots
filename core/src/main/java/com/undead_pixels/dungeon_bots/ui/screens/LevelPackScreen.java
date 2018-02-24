535256fe9a8867cf1e7c6a7181b0d27d01c8f94e
Wesley Oates <wesley.oates@gmail.com>
2018-02-24 15:27:05 -0700

    A level pack screen?

---
+++ core/src/main/java/com/undead_pixels/dungeon_bots/ui/screens/LevelPackScreen.java
@@ -0,0 +1,32 @@
+package com.undead_pixels.dungeon_bots.ui.screens;
+
+import java.awt.Container;
+
+import com.undead_pixels.dungeon_bots.scene.World;
+
+public class LevelPackScreen extends Screen {
+
+	public LevelPackScreen(World world) {
+		super(world);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected ScreenController makeController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addComponents(Container pane) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setDefaultLayout() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
