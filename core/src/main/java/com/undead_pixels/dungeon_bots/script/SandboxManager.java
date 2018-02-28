1268eb5da8d749fe3bc64a4c253e9ba6d188b0ae
lennyferguson <trio.of.four@gmail.com>
2018-02-27 17:16:43 -0700

    Added unpack, and a few additional item types
    Merge branch 'kevin' of https://github.com/cyanpelican/DungeonBots into feature/Inventory
    
    # Conflicts:
    #	core/src/main/java/com/undead_pixels/dungeon_bots/scene/entities/inventory/Inventory.java

--- core/src/main/java/com/undead_pixels/dungeon_bots/script/SandboxManager.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/script/SandboxManager.java
@@ -8,7 +8,7 @@ public class SandboxManager {
 
 	public static void register(Thread runLoopThread, LuaSandbox sandbox) {
 		runLoopThread.setName("lua-"+System.currentTimeMillis()); // TODO - maybe get name of sandbox?
-		
+
 		sandboxes.put(runLoopThread.getName(), sandbox);
 	}
 
@@ -18,7 +18,7 @@ public class SandboxManager {
 	
 	public static LuaSandbox getCurrentSandbox() {
 		String currentThreadName = Thread.currentThread().getName();
-		
+
 		return sandboxes.getOrDefault(currentThreadName, null);
 	}
 
