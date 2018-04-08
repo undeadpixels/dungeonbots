aa544eb6d40167718725de5cc7870979eeb9f6a3
Kevin Parker <kevin.m.parker@gmail.com>
2018-04-08 10:49:24 -0600

    fixed fps bug, but messagepane is inserting images in the wrong spot

--- core/src/main/java/com/undead_pixels/dungeon_bots/scene/LoggingLevel.java
+++ core/src/main/java/com/undead_pixels/dungeon_bots/scene/LoggingLevel.java
@@ -1,8 +1,16 @@
 package com.undead_pixels.dungeon_bots.scene;
 
+import java.awt.Color;
+
 public enum LoggingLevel {
-	STDOUT,
-	GENERAL,
-	QUEST,
-	ERROR,
+	STDOUT(1, Color.white),
+	GENERAL(2, Color.lightGray),
+	QUEST(4, Color.green),
+	ERROR(8, Color.red);
+	
+	public final Color color;
+	
+	private LoggingLevel(int val, Color c) {
+		color = c;
+	}
 }
